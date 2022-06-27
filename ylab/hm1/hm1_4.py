d7afcc04cada1783bff2c53d0df676a0f4f596ed
kazimirovich <big.boy926@gmail.com>
2022-06-27 14:56:51 +0300

    Исправил задание

--- ylab/hm1/hm1_4.py
+++ ylab/hm1/hm1_4.py
@@ -1,22 +1,33 @@
-def bananas(s, word):
-    ret = []
+def bananas(s) -> set:
+    banana = 'banana'
+    if len(s) >= len(banana):
+        return search_banana(s, banana)
+    return set()
 
-    if word == '':
-        ret.append(''.rjust(len(s), '-'))
-        return ret
 
-    left_s = ''
-    for si in range(len(s)):
-        if word[0] == s[si]:
-            left_s = ''.rjust(si, '-') + s[si]
-            if s[si+1:] == '' and word[1:] == '':
-                ret.append(left_s)
+def search_banana(s, banana):
+    result = list()
+    if banana == '':
+        result.append(''.rjust(len(s), '-'))
+        return result
+    for i in range(len(s)):
+        if banana[0] == s[i]:
+            head = ''.rjust(i, '-') + s[i]
+            if s[i+1:] == '' and banana[1:] == '':
+                result.append(head)
             else:
-                right_s_list = bananas(s[si+1:], word[1:])
-                for right_s in right_s_list:
-                    ret.append(left_s + right_s)
-    return ret
+                arr = search_banana(s[i+1:], banana[1:])
+                for tail in arr:
+                    result.append(head + tail)
+    if result == '':
+        result = set()
+    return set(result)
 
-if __name__ == '__main__':
-    for s in bananas('bbananana', 'banana'):
-        print(s)
+
+assert bananas("banana") == {"banana"}
+assert bananas("bbananana") == {"b-an--ana", "-banana--", "-b--anana", "b-a--nana", "-banan--a",
+                     "b-ana--na", "b---anana", "-bana--na", "-ba--nana", "b-anan--a",
+                     "-ban--ana", "b-anana--"}
+assert bananas("bananaaa") == {"banan-a-", "banana--", "banan--a"}
+assert bananas("bananana") == {"ban--ana", "ba--nana", "bana--na", "b--anana", "banana--", "banan--a"}
+assert bananas("banann") == set()
